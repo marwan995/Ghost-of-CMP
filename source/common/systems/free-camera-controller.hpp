f1385c5b7f157d0259dd69b59075fb8397638bf2
MohamedAtef308 <mimo3082002@gmail.com>
2024-05-01 03:34:39 +0300

    Weapons cycling added

--- source/common/systems/free-camera-controller.hpp
+++ source/common/systems/free-camera-controller.hpp
@@ -20,6 +20,10 @@
 
 namespace our
 {
+    // WARNING: this class is used as a player class as it's a FPS game
+
+    // Global constant
+    const float avgDeltaTime = 0.008335638028169f;
 
     // The free camera controller system is responsible for moving every entity which contains a FreeCameraControllerComponent.
     // This system is added as a slightly complex example for how use the ECS framework to implement logic.
@@ -27,15 +31,13 @@ namespace our
     class FreeCameraControllerSystem
     {
         Application *app;          // The application in which the state runs
-        bool mouse_locked = false; // Is the mouse locked
+        
         int activeWeapon = 0;
         int deltasCounter=0;
-        const float weapons_BPS[3] = {8,1,1};   // hold weapons bullets per seconds
-
-        const double avgDeltaTime = 0.008335638028169f;
+        const float weapons_BPS[3] = {10, 1, 1};   // holds weapons bullets per seconds
 
         // utility to return true if a bullet should be spawned
-        bool checkRateOfFire(float deltaTime) {
+        bool checkRateOfFire() {
             // get current weapon BPS
             float bulletsPerSecond = weapons_BPS[activeWeapon];
             // check for it's cooldown
@@ -44,7 +46,7 @@ namespace our
                 deltasCounter++;
                 return true;
             }
-            else if((deltasCounter) * deltaTime >= (1/bulletsPerSecond))
+            else if((deltasCounter) * avgDeltaTime >= (1/bulletsPerSecond))
             {
                 deltasCounter = 0;
             }
@@ -86,61 +88,39 @@ namespace our
             // Get the entity that we found via getOwner of camera (we could use controller->getOwner())
             Entity *entity = camera->getOwner();
 
-
-            // Mouse is always locked
-            ////// If the left mouse button is pressed, we lock and hide the mouse. This common in First Person Games.
-            ////if (app->getMouse().isPressed(GLFW_MOUSE_BUTTON_1) && !mouse_locked)
-            ////{
-            ////    app->getMouse().lockMouse(app->getWindow());
-            ////    mouse_locked = true;
-            ////    // If the left mouse button is released, we unlock and unhide the mouse.
-            ////}
-            ////else if (!app->getMouse().isPressed(GLFW_MOUSE_BUTTON_1) && mouse_locked)
-            ////{
-            ////    app->getMouse().unlockMouse(app->getWindow());
-            ////    mouse_locked = false;
-            ////}
-
             // We get a reference to the entity's position and rotation
             glm::vec3 &position = entity->localTransform.position;
             glm::vec3 &rotation = entity->localTransform.rotation;
             camera->lastPosition = entity->localTransform.position;
 
-            // TODO: if the weapon is changed reset deltasCounter
-
             // Mouse left click (shoot fire)
             if (app->getMouse().isPressed(GLFW_MOUSE_BUTTON_1))
             {
-                if(checkRateOfFire(deltaTime))
+                if(checkRateOfFire())          // if it's time to spawn a bullet or not
                 {
+                    // calculate bullet direction & speed in all 3 directions
+                    float bulletRotation[3] = {180-glm::degrees(rotation.x), glm::degrees(rotation.y)-180, glm::degrees(rotation.z)};
+                    float bulletSpeedX = -cos(-rotation.x)*sin(rotation.y);
+                    float bulletSpeedY = -sin(-rotation.x);
+                    float bulletSpeedZ = -cos(-rotation.x)*cos(rotation.y);
+                    float bulletMovementDirections[3] = {bulletSpeedX, bulletSpeedY, bulletSpeedZ};
+                    float bulletPosition[3] = {position.x + bulletSpeedX/4, position.y + bulletSpeedY/4, position.z + bulletSpeedZ/4};
 
-                float bulletRotation[3] = {180-glm::degrees(rotation.x), glm::degrees(rotation.y)-180, glm::degrees(rotation.z)};
-                float bulletSpeedX = -cos(-rotation.x)*sin(rotation.y);
-                float bulletSpeedY = -sin(-rotation.x);
-                float bulletSpeedZ = -cos(-rotation.x)*cos(rotation.y);
-                float bulletMovementDirections[3] = {bulletSpeedX, bulletSpeedY, bulletSpeedZ};
-                // TODO: make the bullet in front of the player
-                float bulletPosition[3] = {position.x + bulletSpeedX/4, position.y + bulletSpeedY/4, position.z + bulletSpeedZ/4};
-
-                // TODO: can use down casting and polymorphism here
-                // switch(activeWeapon)
-                // {
-                    // laser rifle
-                    // case 0:
+                    if(activeWeapon == 0)
+                    {
+                        // laser rifle
                         LaserBullet* laserBullet = new LaserBullet(bulletPosition, bulletRotation, bulletMovementDirections, world);
                         laserBullet->isFriendly = true;
                         laserBullet->shoot();
-                //     break;
-                    
-                //     case 1:
-                //     std::cout<<"\n";
-                //     break;
-
-                //     case 2:
-                //     std::cout<<"\n";
-                //     break;
-                // }
-                // TODO: move this code to the bullet file
+                    }
+                    else if(activeWeapon == 1)
+                    {
+                        std::cout<<"weapon2\n";
+                    }
+                    else if(activeWeapon == 2)
+                    {
+                        std::cout<<"weapon3\n";
+                    }
                 }
             }
 
@@ -148,24 +128,23 @@ namespace our
             // and use it to update the camera rotation
             
             // Mouse movement is always checked
-            ////if (app->getMouse().isPressed(GLFW_MOUSE_BUTTON_1))
-            ////{
-                glm::vec2 delta = app->getMouse().getMouseDelta();
-                rotation.x -= delta.y * controller->rotationSensitivity; // The y-axis controls the pitch
-                rotation.y -= delta.x * controller->rotationSensitivity; // The x-axis controls the yaw
-            ////}
+            glm::vec2 delta = app->getMouse().getMouseDelta();
+            rotation.x -= delta.y * controller->rotationSensitivity; // The y-axis controls the pitch
+            rotation.y -= delta.x * controller->rotationSensitivity; // The x-axis controls the yaw
 
             // We prevent the pitch from exceeding a certain angle from the XZ plane to prevent gimbal locks
             if (rotation.x < -glm::half_pi<float>() * 0.99f)
                 rotation.x = -glm::half_pi<float>() * 0.99f;
             if (rotation.x > glm::half_pi<float>() * 0.99f)
                 rotation.x = glm::half_pi<float>() * 0.99f;
+
             // This is not necessary, but whenever the rotation goes outside the 0 to 2*PI range, we wrap it back inside.
             // This could prevent floating point error if the player rotates in single direction for an extremely long time.
             rotation.y = glm::wrapAngle(rotation.y);
             position.y = glm::clamp(position.y, 0.0f, max_y);
 
             // We update the camera fov based on the mouse wheel scrolling amount
+            // TODO: change fov on right mouse click
             float fov = camera->fovY + app->getMouse().getScrollOffset().y * controller->fovSensitivity;
             fov = glm::clamp(fov, glm::pi<float>() * 0.01f, glm::pi<float>() * 0.99f); // We keep the fov in the range 0.01*PI to 0.99*PI
             camera->fovY = fov;
@@ -178,11 +157,12 @@ namespace our
                       right = glm::vec3(matrix * glm::vec4(1, 0, 0, 0));
 
             glm::vec3 current_sensitivity = controller->positionSensitivity;
+
             // If the LEFT SHIFT key is pressed, we multiply the position sensitivity by the speed up factor
             if (app->getKeyboard().isPressed(GLFW_KEY_LEFT_SHIFT))
                 current_sensitivity *= controller->speedupFactor;
 
-            // We change the camera position based on the keys WASD/QE
+            // We change the camera position based on the keys WASD
             // S & W moves the player back and forth
             // forward/backward motion
             // Project y-component to x-z plane
@@ -196,13 +176,6 @@ namespace our
             }
             if (app->getKeyboard().isPressed(GLFW_KEY_S))
                 position -= forwardMotion * (deltaTime * current_sensitivity.z);
-            
-            // Not used in our case
-            //// Q & E moves the player up and down
-            //// if (app->getKeyboard().isPressed(GLFW_KEY_Q))
-            ////     position += up * (deltaTime * current_sensitivity.y);
-            //// if (app->getKeyboard().isPressed(GLFW_KEY_E))
-            ////     position -= up * (deltaTime * current_sensitivity.y);
             
             // A & D moves the player left or right
             if (app->getKeyboard().isPressed(GLFW_KEY_D))
@@ -231,15 +204,29 @@ namespace our
                 verticalVelocity = 0.0f; // Reset vertical velocity
                 isJumping = false;       // Reset jump flag
             }
+
+            // Check for weapon change
+            if(app->getKeyboard().isPressed(GLFW_KEY_1) && activeWeapon != 0)
+            {
+                activeWeapon = 0;
+                deltasCounter = 0;
+            }
+            else if (app->getKeyboard().isPressed(GLFW_KEY_2) && activeWeapon != 1)
+            {
+                activeWeapon = 1;
+                deltasCounter = 0;
+            }
+            else if (app->getKeyboard().isPressed(GLFW_KEY_3) && activeWeapon != 2)
+            {
+                activeWeapon = 2;
+                deltasCounter = 0;
+            }
         }
+
         // When the state exits, it should call this function to ensure the mouse is unlocked
         void exit()
         {
-            //// if (mouse_locked)
-            //// {
-            ////    mouse_locked = false;
                 app->getMouse().unlockMouse(app->getWindow());
-            //// }
         }
     };
 }
